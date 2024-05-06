7fd4bb17120a884d9aaba3af3902e6311fc990b7
wanshmain <wanshmain@gmail.com>
2024-05-07 04:54:09 +0530

    initial commit

---
+++ lib/game/assets.dart
@@ -0,0 +1,13 @@
+class Assets{
+  static const background ='background.png';
+  static const ground ='ground.png';
+  static const clouds ='clouds.png';
+  static const pipe ='pipe.png';
+  static const piperotated ='pipe_rotated.png';
+  static const birdmidflap ='bird_midflap.png';
+  static const birdupflap ='bird_upflap.png';
+  static const birddownflap ='bird_downflap.png';
+  static const gameOver ='gameover.png';
+  static const menu ='menu.png';
+  static const message ='message.png';
+}
